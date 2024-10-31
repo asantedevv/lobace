b1fde97d2b6ac550c184d8e9ec145584bf7462a9
gr8kelvin <gr8kelvin@gmail.com>
2024-10-31 15:44:18 +0000

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,45 @@
+import 'package:final_food/screens/account_screen.dart';
+import 'package:final_food/screens/add_new_card.dart';
+import 'package:final_food/screens/address_screen.dart';
+import 'package:final_food/screens/carry_out.dart';
+import 'package:final_food/screens/change_password.dart';
+import 'package:final_food/screens/dashboard.dart';
+import 'package:final_food/screens/delete_profile_screen.dart';
+import 'package:final_food/screens/delivery.dart';
+import 'package:final_food/screens/food_screen.dart';
+import 'package:final_food/screens/forget_password.dart';
+import 'package:final_food/screens/location_screen.dart';
+import 'package:final_food/screens/login_screen.dart';
+import 'package:final_food/screens/main_screen.dart';
+import 'package:final_food/screens/orders_account.dart';
+import 'package:final_food/screens/payment_screen.dart';
+import 'package:final_food/screens/rating_and_reviews.dart';
+import 'package:final_food/screens/restaurant_screen.dart';
+import 'package:final_food/screens/sign_in_screen.dart';
+import 'package:final_food/screens/sign_up_screen.dart';
 import 'package:final_food/screens/splash_screen.dart';
+import 'package:final_food/screens/track_order.dart';
+import 'package:final_food/screens/update_profile_screen.dart';
+import 'package:final_food/screens/users_screen.dart';
 import 'package:flutter/material.dart';
+
 
 void main() {
   runApp(const MyApp());
 }
+
+
+
+// void main() {
+//   runApp(
+//     MultiProvider(
+//       providers: [
+//         ChangeNotifierProvider(create: (_) => FoodCartProvider()),
+//       ],
+//       child: MyApp(),
+//     ),
+//   );
+// }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -12,6 +48,37 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      initialRoute: '/',
+      routes: {
+        '/signInScreen': (context) => const SignInScreen(),
+        '/splashScreen': (context) => const SplashScreen(),
+        '/signUpScreen': (context) => const SignUpScreen(),
+        '/loginScreen': (context) => const LoginScreen(),
+        '/forgetPassword': (context) => const ForgetPassword(),
+        '/mainScreen': (context) => const MainScreen(),
+        '/delivery': (context) => const Delivery(),
+        '/carryOut': (context) => const CarryOut(),
+        '/orders': (context) => const OrdersAccount(),
+        '/payment': (context) => const PaymentScreen(),
+        '/addCard': (context) => const AddNewCard(),
+        '/address': (context) => const AddressScreen(),
+        '/location': (context) => const LocationScreen(),
+        '/rating': (context) => const RatingAndReviews(),
+        '/logout': (context) => const SignInScreen(),
+        '/changePassword': (context) => const ChangePassword(),
+        '/account': (context) => const AccountScreen(),
+        '/tracker': (context) => const TrackOrder(),
+        '/dashboard': (context) => const Dashboard(),
+        '/foodScreen': (context) => const FoodScreen(),
+        '/usersScreen': (context) => const UsersScreen(),
+        '/updateProfileScreen': (context) => const UpdateProfileScreen(),
+        '/deleteProfileScreen': (context) => const DeleteProfileScreen(),
+        '/restaurantScreen': (context) => RestaurantScreen(
+              id: 0,
+            ),
+        // '/oderSummary': (context) => OrderSummaryScreen(),
+      },
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         // This is the theme of your application.
@@ -33,94 +100,6 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       home: const SplashScreen(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
